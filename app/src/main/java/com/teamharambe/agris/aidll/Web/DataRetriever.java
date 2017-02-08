2ba0b989942f1ff95aec549efddf5d7c32d8a10c
AigarsN <aigars.norkus@gmail.com>
2017-02-08 20:52:25 +0200

    Test commit

--- app/src/main/java/com/teamharambe/agris/aidll/Web/DataRetriever.java
+++ app/src/main/java/com/teamharambe/agris/aidll/Web/DataRetriever.java
@@ -28,7 +28,7 @@ private DataRetrieverResponse listener;
 //        progressBar = new ProgressBar(AIDLL.getAppContext());
 //        progressBar.setVisibility(View.VISIBLE);
     }
-//AyyLe.mov
+//AyyLe.mov 2
     @Override
     protected String doInBackground(Void... params) {
         try {
