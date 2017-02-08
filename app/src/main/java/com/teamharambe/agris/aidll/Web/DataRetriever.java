a344764324d1c027940fa1200558511bbefd7b61
AigarsN <aigars.norkus@gmail.com>
2017-02-08 20:55:43 +0200

    Revert "Test commit"
    This reverts commit 2ba0b989942f1ff95aec549efddf5d7c32d8a10c.
    
    # Conflicts:
    #	app/src/main/java/com/teamharambe/agris/aidll/Web/DataRetriever.java

--- app/src/main/java/com/teamharambe/agris/aidll/Web/DataRetriever.java
+++ app/src/main/java/com/teamharambe/agris/aidll/Web/DataRetriever.java
@@ -28,7 +28,6 @@ private DataRetrieverResponse listener;
 //        progressBar = new ProgressBar(AIDLL.getAppContext());
 //        progressBar.setVisibility(View.VISIBLE);
     }
-    //// TODO: 2017.02.08.  remove this comment
 //AyyLe.mov
     @Override
     protected String doInBackground(Void... params) {
